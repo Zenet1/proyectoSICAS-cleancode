8a0b937f1fbe5392b0bf10aaf75ede4edcff3cd1
Ednover <ed_leo1411@hotmail.com>
2023-12-10 14:19:54 -0600

    Add functions in SessionController

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,16 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-import { Form, FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup } from '@angular/forms';
 import { Router } from '@angular/router';
 import { CookieService } from 'src/app/services/cookie/cookie.service';
-import { LoginService } from 'src/app/services/login/login.service';
-import { LoginController } from 'src/app/sesion/infraestructure/SessionController';
+import { SessionController } from 'src/app/sesion/infraestructure/SessionController';
 import { AccountDataDTO } from 'src/app/sesion/domain/dto/AccountDataDTO';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css'],
-  providers: [LoginController],
+  providers: [SessionController],
 })
 export class LoginComponent implements OnInit {
   estaLogueado: boolean;
@@ -18,83 +17,61 @@ export class LoginComponent implements OnInit {
   facultades: any;
 
   constructor(
-    private servicioLogin: LoginService,
     private servicioCookie: CookieService,
     private formBuilder: FormBuilder,
     private router: Router,
-    private loginController: LoginController
+    private sessionController: SessionController 
   ) {}
 
   ngOnInit(): void {
-    this.estaLogueado = this.servicioLogin.isLoggedIn();
     this.servicioCookie.deleteCookie('cuestionarioContestado');
+    this.estaLogueado = this.sessionController.isLoggedIn();
+    const session = this.sessionController.getItem();
     if (this.estaLogueado) {
-      switch (this.servicioLogin.getRol()) {
-      case 'Alumno': {
-        this.router.navigateByUrl('inicio-alumno');
-        break;
-      }
-      case 'Administrador': {
-        this.router.navigateByUrl('inicio-administrador');
-        break;
-      }
-      case 'Capturador': {
-        this.router.navigateByUrl('inicio-capturador');
-        break;
-      }
-      case 'Profesor': {
-        this.router.navigateByUrl('inicio-personal');
-        break;
-      }
-      case 'Personal': {
-        this.router.navigateByUrl('inicio-personal');
-        break;
-      }
-      default: {
-        this.router.navigateByUrl('login');
-      }
-      }
+      this.routerHandle(session.Rol);
     } else {
-      this.formLogin = this.formBuilder.group({
-        usuario: [''],
-        contrasena: [''],
-      });
+      this.resetFields();
     }
   }
-
-  trimCampo(campo: any, valor: any) {
-    const textoTrim = valor.trim();
-    campo.setValue(textoTrim);
-  }
-
-  trimForm() {
-    this.trimCampo(
-      this.formLogin.controls['usuario'],
-      this.formLogin.controls['usuario'].value
-    );
-  }
-
-  async iniciarSesion() {
+  
+  public async login() {
     this.trimForm();
     try {
-      const response: AccountDataDTO = await this.loginController.find(this.formLogin.value);
-      sessionStorage.setItem('usuario', JSON.stringify(response));
+      const response: AccountDataDTO = await this.sessionController.login(this.formLogin.value);
       this.routerHandle(response.Rol);
     } catch (error) {
       if (error instanceof Error) {
         if(error.message === 'User Not Found') {
           alert('Usuario o contraseña incorrectos');
         } else if (error.message === 'User Is Ban') {
-          alert('El usuario esta baneado');
+          alert('Su usuario esta baneado');
         }
       } else {
-        // Otro tipo de error, puedes manejarlo de acuerdo a tus necesidades
         alert('Error durante la autenticación');
       }
     }
   }
 
-  routerHandle (rol: string) {
+  private trimCampo(campo: any, valor: any) {
+    const textoTrim = valor.trim();
+    campo.setValue(textoTrim);
+  }
+
+  private trimForm() {
+    this.trimCampo(
+      this.formLogin.controls['usuario'],
+      this.formLogin.controls['usuario'].value
+    );
+  }
+
+  private resetFields () {
+    this.formLogin = this.formBuilder.group({
+      usuario: [''],
+      contrasena: [''],
+    });
+  }
+
+  private routerHandle (rol: string) {
     const routes = {
       Alumno: 'inicio-alumno',
       Capturador: 'inicio-capturador',
--- src/app/sesion/infraestructure/SessionController.ts
+++ src/app/sesion/infraestructure/SessionController.ts
@@ -4,10 +4,23 @@ import { LoginCase } from '../domain/usecase/SesionCase';
 import { IncidentGateway } from './IncidentGateway';
 import { LoginGateway } from './LoginGateway';
 
-export class LoginController {
-  async find(credentials: CredentialsDTO): Promise<AccountDataDTO> {
-    const res = await new LoginCase(new LoginGateway(), new IncidentGateway())
+export class SessionController {
+  public async login(credentials: CredentialsDTO): Promise<AccountDataDTO> {
+    const response = await new LoginCase(new LoginGateway(), new IncidentGateway())
       .StartSesion(credentials);
-    return res;
+    sessionStorage.setItem('usuario', JSON.stringify(response));
+    return response;
+  }
+
+  public logout () {
+    sessionStorage.removeItem('usuario');
+  }
+
+  public getItem (): AccountDataDTO {
+    return JSON.parse(sessionStorage.getItem('usuario'));
+  }
+
+  public isLoggedIn (): boolean {
+    return (this.getItem()) ? true : false;
   }
 }
